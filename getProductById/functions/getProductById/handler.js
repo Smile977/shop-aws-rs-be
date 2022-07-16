53c8f72cadf946921d2a8e6c1d30b595835733e1
Andrei Biareshchanka <Andrei.Biareshchanka@coxautoinc.com>
2022-07-16 06:48:26 +0400

    added product

--- getProductById/functions/getProductById/handler.js
+++ getProductById/functions/getProductById/handler.js
@@ -5,8 +5,11 @@ module.exports.getProductById = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        product: '1 book from array',
-        price: 12345,
+        count: 6,
+        description: "Short Product Description3",
+        id: "7567ec4b-b10c-48c5-9345-fc73c48a80a0",
+        price: 10,
+        title: "ProductNew"
       },
     ),
   };
